d3c74580b8c026f8c1ea66eaca75be35be804e9f
Tanya Singh <98661040+Tanya19Singh@users.noreply.github.com>
2025-03-04 14:31:39 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const cryptoJS = require('crypto-js');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ||5000;
 
 app.use(bodyParser.json());
 app.use(cors({
